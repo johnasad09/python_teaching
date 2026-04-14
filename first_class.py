7e9ec43297a5358679302cc654af8ffa683dd0ca
Asad Ullah <johnasad09@gmail.com>
2026-04-14 23:45:55 +0500

    Implement basic Python operations in first_class.py
    Added a script demonstrating various Python operations including arithmetic, boolean, and type casting.

---
+++ first_class.py
@@ -0,0 +1,62 @@
+"""
+multi - line comment
+the print function prints
+the output the console
+"""
+
+# print("hello asad")
+# single line comment
+# ctrl /
+
+# Arithematic Operations
+# operator, operand
+# print(1-(1+5)-3+6/(8*2))
+# PMDASLR
+
+# floor division
+# print(5/3)
+# print(5//5)
+# print(-5//3)
+
+# modulo operation % - remainder
+# print(5%3)
+
+# exponentiation
+# print(3**5)
+
+# boolean
+# print(True)
+# print(False)
+# print(True and False)
+# print(True or False)
+
+# print(True + True)
+# print(True + False)
+
+# type casting
+# print(int(2.025))
+# print(float(8))
+# print(str())
+
+# print(int(True))
+# print(int(False))
+# print(True + True + True + True)
+#
+# print(True and True)
+# print((True + True) * 9)
+
+# Comparison operators
+# print(2==2)
+# print(3 != 4)
+# print(5 >= 4)
+# print(5 <= 4)
+# name = input("what is your name?")
+# print("Hello " + name + "How are you?") #concatanation
+# print(f"Hello {name}! How are you?")
+
+# input function accepts value as a string, so we have to convert it into int
+value1 = int(input("enter your first value"))
+value2 = int(input("enter your second value"))
+total = value1 - value2
+print(f"Your total is {total}")
+# print(type(value1))
