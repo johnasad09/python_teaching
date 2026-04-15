1ca0b744e2c17c00143421942743a4071d7f4eb6
Asad Ullah <johnasad09@gmail.com>
2026-04-15 23:36:25 +0500

    Implement string functions and user input examples
    Add examples of string functions, slicing, and user input.

---
+++ second_class.py
@@ -0,0 +1,34 @@
+# length function
+# len()
+# print(len("ASAD"))
+
+# index
+greet = 'Welcome!'
+# print(greet[1])
+# print(greet[-1])
+# print(greet[-3]) #m
+
+# slicing
+# [start:end:step]
+# print(greet[3:6])
+# print(greet[0:1])
+# print(greet[0:6])
+# print(greet[:6]) # from start to index 6
+# print(greet[2:]) # from 2nd to last
+# numbers = "123456789"
+# print(numbers[1:6:1])
+# print(numbers[::3]) # steps
+
+# large integer
+# print(123_456_789 + 1)
+
+# live assignment
+# take an input from a username, and print its length
+user_name = input("Enter your name")
+print(f"The length of your name is {len(user_name)}")
+
+# assignment homework
+# bmi calculate
+# bmi formula
+# height weight
+# prints the bmi
