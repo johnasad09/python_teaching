ad4dac2d6dd0dd9430d1f234eb02d941c36eea1c
Asad Ullah <johnasad09@gmail.com>
2026-04-20 23:29:34 +0500

    Create 5th_class.py

---
+++ 5th_class.py
@@ -0,0 +1,68 @@
+
+# fruit1 = "apple"
+# fruit2 = 'mango'
+# fruit3 = 'banana'
+
+# list
+# fruits = ['apple', 'mango', 'banana', 2, 10.0]
+# print(fruits[2])
+# out of index error
+# print(fruits[5])
+# print(fruits[-3])
+#
+# # slicing
+# print(fruits[1:2])
+
+# append
+# adds value to the end of a list
+# numbers = [1, 2, 3, 4]
+# print(numbers)
+# numbers.append(6)
+# print(numbers)
+# # insert
+# numbers.insert(2, "three")
+# print(numbers)
+# numbers.pop(0)
+# print(numbers)
+# numbers.remove(4)
+# print(numbers)
+# numbers.remove(4)
+# print(numbers)
+# numbers = [1, 2, 3, 4]
+# digits = numbers
+# print(digits)
+# print(digits is numbers)
+# print(id(digits))
+# print(id(numbers))
+# copy the items of a list
+# list_numbers = numbers.copy()
+# print(list_numbers)
+# print(id(list_numbers))
+# print(list_numbers is numbers)
+# numbers.reverse()
+# print(numbers)
+
+
+# Dice rolling
+# import random
+# dice = ['one', 'two', 'three', 'four', 'five', 'six']
+# print(random.choice(dice))
+#
+# dice_index = random.randint(0,5)
+# print(dice[dice_index])
+
+# nested-list
+# fruits = ["apple", "banana", "cherry"]
+# vegetables = ["carrot", 'spinach', 'turnip']
+# mixed = [fruits, vegetables]
+# print(mixed[1][1])
+
+# mixed = [
+#     ['apple', 'banana', 'cherry'],
+#     ['carrot', 'spinach', 'turnip']
+# ]
+
+# mixed = [['apple', 'banana', 'cherry'],['carrot', 'spinach', 'turnip']]
+# print(mixed[1][1])
+
+# Rock, scissor, paper
