1059ad4c0644786e9c740717a195c63b7fa904cf
Asad Ullah <johnasad09@gmail.com>
2026-04-22 22:57:50 +0500

    Create 7th_class.py

---
+++ 7th_class.py
@@ -0,0 +1,43 @@
+# FOR LOOP
+fruits = ['mango', 'banana', 'apple', 'melon', 'guava']
+
+for fruit in fruits:
+    print(fruit)
+    if fruit == 'apple':
+        print(fruit + " cake")
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+for num in numbers:
+    if num % 2 == 0:
+        print(num ** 3)
+
+print("-" * 20)
+numbers = [1,2,3,4,5,6,7,8,9,10]
+for num in range(1,11,2):
+    print(num)
+print("hello")
+    if num % 2 == 0:
+        print(num ** 3)
+
+
+
+numbers = [1,2,3,4,0,6,7,8,9,10]
+finds the maximum number in a sequence
+print(max(numbers))
+finds the minimum value in a sequence
+print(min(numbers))
+print(sum(numbers))
+
+score = [50,200,210,33,45,180]
+total = 0
+for num in score:
+    total += num
+print(total)
+
+print(sum(score))
+
+# ASSIGNMENT
+# print your name 10 times.
+# input() - username
+# print() - how many times do you want to print your name.
+# 5
