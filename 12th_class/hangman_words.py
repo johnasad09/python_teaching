2695d8505ba099639e90b07bc2a0ac8667692a7b
Asad Ullah <johnasad09@gmail.com>
2026-05-08 00:03:24 +0500

    Create hangman_words.py

---
+++ 12th_class/hangman_words.py
@@ -0,0 +1,18 @@
+word_list = [
+    "algorithm", "binary", "boolean", "compiler", "database", 
+    "encryption", "function", "hardware", "internet", "java", 
+    "keyboard", "laptop", "monitor", "network", "operator", 
+    "python", "protocol", "runtime", "software", "variable",
+    "avalanche", "blizzard", "canyon", "desert", "evergreen", 
+    "forest", "glacier", "horizon", "island", "jungle", 
+    "kangaroo", "mountain", "ocean", "plateau", "river", 
+    "safari", "thunder", "volcano", "waterfall", "zebra",
+    "abruptly", "absurd", "abyss", "awkward", "bagpipes", 
+    "banjo", "beekeeper", "blitz", "buzzard", "crypt", 
+    "dwarf", "faking", "fishhook", "fjord", "galaxy", 
+    "gossip", "haiku", "haphazard", "ivory", "jackpot", 
+    "jelly", "jigsaw", "jinx", "knapsack", "luxury", 
+    "pajamas", "phlegm", "pixel", "quartz", "queue", 
+    "rhythm", "scratch", "sphinx", "thriftless", "unknown", 
+    "vaporize", "vortex", "waltz", "wyvern", "xylophone"
+]
